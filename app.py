0a52d97e639bede528497e67d5ad4cac70c587d7
oreo21 <reok99@gmail.com>
2016-10-10 13:28:02 -0400

    beautified code syntax

--- app.py
+++ app.py
@@ -4,33 +4,37 @@ from utils import auth
 app = Flask(__name__)
 app.secret_key = "nine"
 
+
 @app.route("/")
 def home():
-	print request
-	if ('user' in session.keys() and auth.userExists(session['user'])):
-		mHome = "Welcome back " + session['user'] + "!"
-		return render_template('home.html', messageHome = mHome)
-	else:
-		return redirect(url_for('login'))
+    print request
+    if ('user' in session.keys() and auth.userExists(session['user'])):
+        mHome = "Welcome back " + session['user'] + "!"
+        return render_template('home.html', messageHome=mHome)
+    else:
+        return redirect(url_for('login'))
+
 
 @app.route("/login")
 def login():
     return render_template('login.html')
 
-@app.route("/authenticate", methods = ['POST'])
-def authenticate():
-	u = request.form['username']
-	p = request.form['password']
-	a = request.form['action']
-	if (a == 'Login' or a == 'Register'):
-		return auth.task(u, p, a)
-	else:
-		return redirect(url_for('login'))
 
-@app.route("/logout", methods = ['POST'])
+@app.route("/authenticate", methods=['POST'])
+def authenticate():
+    u = request.form['username']
+    p = request.form['password']
+    a = request.form['action']
+    if (a == 'Login' or a == 'Register'):
+        return auth.task(u, p, a)
+    else:
+        return redirect(url_for('login'))
+
+
+@app.route("/logout", methods=['POST'])
 def logout():
-	session.pop('user')
-	return redirect(url_for('home'))
+    session.pop('user')
+    return redirect(url_for('home'))
 
 if __name__ == "__main__":
     app.debug = True
--- utils/auth.py
+++ utils/auth.py
@@ -1,55 +1,64 @@
-from flask import render_template, render_template, url_for, request, redirect, session
+from flask import render_template, url_for, request, redirect, session
 import hashlib
 
+
 def scanCSV(file):
-	instream = open(file,'r')
-	content = instream.read().strip()
-	instream.close()
-	return content
+    instream = open(file, 'r')
+    content = instream.read().strip()
+    instream.close()
+    return content
+
 
 def appendCSV(file, string):
-	outstream = open(file,'a')
-	outstream.write(string)
-	outstream.close()
+    outstream = open(file, 'a')
+    outstream.write(string)
+    outstream.close()
+
 
 def genList(string):
-	L1 = string.split("\n")
-	L2 = []
-	for a in L1:
-		a = a.split(",")
-		L2 += [a]
-	return L2
+    L1 = string.split("\n")
+    L2 = []
+    for a in L1:
+        a = a.split(",")
+        L2 += [a]
+    return L2
+
 
 def userExists(username):
-	userList = genList(scanCSV('data/users.csv'))
-	ans = False
-	for account in userList:
-		if username == account[0]:
-			ans = True
-	return ans
+    userList = genList(scanCSV('data/users.csv'))
+    ans = False
+    for account in userList:
+        if username == account[0]:
+            ans = True
+    return ans
+
+
+def hashPass(password):
+    return hashlib.md5(password).hexdigest()
+
 
 def task(username, password, action):
-	userList = genList(scanCSV('data/users.csv'))
-	if action == 'Login':
-		for account in userList:
-			if (username == account[0] and hashlib.md5(password).hexdigest() == account[1]):
-				session['user'] = username
-				return redirect(url_for('home'))
-			if (username == account[0] and not hashlib.md5(password).hexdigest() == account[1]):
-				m = "Incorrect password."
-				return render_template("login.html", messageLogin = m)
-		m = "Username does not exist."
-		return render_template("login.html", messageLogin = m)
-	else:
-		exists = False
-		for account in userList:
-			if username == account[0]:
-				exists = True
-		if exists:
-			m = "The username already exists."
-			return render_template("login.html", messageLogin = m)
-		else:
-			newUser = username + "," + hashlib.md5(password).hexdigest() + "\n"
-			appendCSV('data/users.csv', newUser)
-			m = "Account successfully created."
-			return render_template("login.html", messageLogin = m)
+    userList = genList(scanCSV('data/users.csv'))
+    if action == 'Login':
+        for account in userList:
+            if (username == account[0] and hashPass(password) == account[1]):
+                session['user'] = username
+                return redirect(url_for('home'))
+            if (username == account[0] and not hashPass(password) == account[1]):
+                m = "Incorrect password."
+                return render_template("login.html", messageLogin=m)
+        m = "Username does not exist."
+        return render_template("login.html", messageLogin=m)
+    else:
+        exists = False
+        for account in userList:
+            if username == account[0]:
+                exists = True
+        if exists:
+            m = "The username already exists."
+            return render_template("login.html", messageLogin=m)
+        else:
+            newUser = username + "," + hashPass(password) + "\n"
+            appendCSV('data/users.csv', newUser)
+            m = "Account successfully created."
+            return render_template("login.html", messageLogin=m)
